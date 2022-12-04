1e2ffff8efb0dead2e668129bda4c0d0d560d5af
dhiru0904 <dhirajdevre4@gmail.com>
2022-12-04 15:40:40 +0530

    Added: Start with stake of game every day  and every game

---
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -0,0 +1,13 @@
+package com.gamblingsimulation;
+/*
+crate class
+ */
+public class GamblingSimulation {
+    static int stake = 100;
+    static int bet = 1;
+    public static void main(String[] args) {
+        System.out.println("welcome to Gambling Simulation Problem");
+        System.out.println("Gambler every day stake is " + stake + "and bet is " + bet);
+    }
+}
+
