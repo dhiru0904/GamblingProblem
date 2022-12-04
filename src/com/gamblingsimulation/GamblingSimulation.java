6d89b0a714bd1c00a8155b26ce83fea0765e14d0
dhiru0904 <dhirajdevre4@gmail.com>
2022-12-04 16:02:24 +0530

    Added: gambler win or loss the game make  betor loss

--- src/com/gamblingsimulation/GamblingSimulation.java
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -1,13 +1,33 @@
 package com.gamblingsimulation;
+import java.util.Random;
 /*
 crate class
  */
 public class GamblingSimulation {
     static int stake = 100;
     static int bet = 1;
+    /*
+     win or loss the game
+     */
+    public static void winLoss()
+    {
+        Random random=new Random();
+        int bet=random.nextInt(2);
+        if (bet==1)
+        {
+            System.out.println("Gambler won the bet");
+            stake += bet;
+        }
+        else
+        {
+            stake -= bet;
+            System.out.println("Gambler loose the bet");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("welcome to Gambling Simulation Problem");
         System.out.println("Gambler every day stake is " + stake + "and bet is " + bet);
+        winLoss();
     }
 }
 
